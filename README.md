ca64fbad1ca3ffcc4704ec09350f4683b1fa99c3
tt <894808578@qq.com>
2020-01-13 16:38:06 +0800

    new push test

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-"# react test" 
+#testing
+new push test
---
+++ script.js
@@ -0,0 +1,12 @@
+function square(x){
+  var ans = x*x;
+  return ans;
+}
+
+function myFunction()
+{
+  var userName = "caiyitao";
+  document.getElementById("email").innerHTML=userName+"@pku.edu.cn";
+}
+
+var x = 3;
